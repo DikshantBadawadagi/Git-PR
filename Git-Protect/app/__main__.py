4c466cdccadea7f8c640bbbb2caf3e57f0e9d9af
Dikshant <dikshantbadawadagi@gmail.com>
2024-11-22 02:20:24 +0530

    Initial Commit

---
+++ Git-Protect/app/__main__.py
@@ -0,0 +1,77 @@
+import asyncio
+import importlib
+import os
+import sys
+import traceback
+
+import aiohttp
+import cachetools
+import sentry_sdk
+from aiohttp import web
+from gidgethub import aiohttp as gh_aiohttp
+from gidgethub import apps, routing, sansio
+
+from . import backport, close_pr, filepaths, gh_issue, news, stage
+
+router = routing.Router(
+    backport.router,
+    gh_issue.router,
+    close_pr.router,
+    filepaths.router,
+    news.router,
+    stage.router,
+)
+cache = cachetools.LRUCache(maxsize=500)
+
+sentry_sdk.init(os.environ.get("SENTRY_DSN"))
+
+
+async def main(request):
+    try:
+        body = await request.read()
+        secret = os.environ.get("GH_SECRET")
+        event = sansio.Event.from_http(request.headers, body, secret=secret)
+        print("GH delivery ID", event.delivery_id, file=sys.stderr)
+        if event.event == "ping":
+            return web.Response(status=200)
+
+        async with aiohttp.ClientSession() as session:
+            gh = gh_aiohttp.GitHubAPI(session, "python/app", cache=cache)
+            if not event.data.get("installation"):
+                return web.Response(text="Must be installed as an App.", status=400)
+            installation_id = event.data["installation"]["id"]
+            installation_access_token = await apps.get_installation_access_token(
+                gh,
+                installation_id=installation_id,
+                app_id=os.environ.get("GH_APP_ID"),
+                private_key=os.environ.get("GH_PRIVATE_KEY"),
+            )
+            gh.oauth_token = installation_access_token["token"]
+
+            # Give GitHub some time to reach internal consistency.
+            await asyncio.sleep(1)
+            await router.dispatch(event, gh, session=session)
+        try:
+            print("GH requests remaining:", gh.rate_limit.remaining)
+        except AttributeError:
+            pass
+        return web.Response(status=200)
+    except Exception as exc:
+        traceback.print_exc(file=sys.stderr)
+        return web.Response(status=500)
+
+
+@router.register("installation", action="created")
+async def repo_installation_added(event, gh, *args, **kwargs):
+    print(
+        f"App installed by {event.data['installation']['account']['login']}, installation_id: {event.data['installation']['id']}"
+    )
+
+
+if __name__ == "__main__":  # pragma: no cover
+    app = web.Application()
+    app.router.add_post("/", main)
+    port = os.environ.get("PORT")
+    if port is not None:
+        port = int(port)
+    web.run_app(app, port=port)
---
+++ Git-Protect/tests/test___main__.py
@@ -0,0 +1,85 @@
+from unittest import mock
+
+from aiohttp import web
+from gidgethub import sansio
+
+from app import __main__ as main
+
+app_installation_payload = {
+    "installation": {
+        "id": 123,
+        "account": {"login": "mariatta"},
+    }
+}
+
+
+async def test_ping(aiohttp_client):
+    app = web.Application()
+    app.router.add_post("/", main.main)
+    client = await aiohttp_client(app)
+    headers = {"x-github-event": "ping", "x-github-delivery": "1234"}
+    data = {"zen": "testing is good"}
+    response = await client.post("/", headers=headers, json=data)
+    assert response.status == 200
+
+
+async def test_bad_request_if_no_installation(aiohttp_client):
+    app = web.Application()
+    app.router.add_post("/", main.main)
+    client = await aiohttp_client(app)
+    headers = {"x-github-event": "project", "x-github-delivery": "1234"}
+    # Sending a payload that shouldn't trigger any networking, but no errors
+    # either.
+    data = {"action": "created"}
+    response = await client.post("/", headers=headers, json=data)
+    assert response.status == 400
+    assert await response.text() == "Must be installed as an App."
+
+
+async def test_failure(aiohttp_client):
+    """Even in the face of an exception, the server should not crash."""
+    app = web.Application()
+    app.router.add_post("/", main.main)
+    client = await aiohttp_client(app)
+    # Missing key headers.
+    response = await client.post("/", headers={})
+    assert response.status == 500
+
+
+@mock.patch("gidgethub.apps.get_installation_access_token")
+async def test_success_with_installation(get_access_token_mock, aiohttp_client):
+    get_access_token_mock.return_value = {
+        "token": "ghs_blablabla",
+        "expires_at": "2023-06-14T19:02:50Z",
+    }
+    app = web.Application()
+    app.router.add_post("/", main.main)
+    client = await aiohttp_client(app)
+    headers = {"x-github-event": "project", "x-github-delivery": "1234"}
+    # Sending a payload that shouldn't trigger any networking, but no errors
+    # either.
+    data = {"action": "created"}
+    data.update(app_installation_payload)
+    response = await client.post("/", headers=headers, json=data)
+    assert response.status == 200
+
+
+class FakeGH:
+    def __init__(self):
+        pass
+
+
+async def test_repo_installation_added(capfd):
+    event_data = {
+        "action": "created",
+    }
+    event_data.update(app_installation_payload)
+
+    event = sansio.Event(event_data, event="installation", delivery_id="1")
+    gh = FakeGH()
+    await main.router.dispatch(event, gh)
+    out, err = capfd.readouterr()
+    assert (
+        f"App installed by {event.data['installation']['account']['login']}, installation_id: {event.data['installation']['id']}"
+        in out
+    )
